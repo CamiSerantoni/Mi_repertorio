5927b112ba76814ccb46f591329d7a71fd087a6b
Camila Serantoni <camila.serantoni@blue.cl>
2024-05-18 15:37:04 -0400

    limpia el código

--- config/db.js
+++ config/db.js
@@ -2,7 +2,6 @@ import pkg from 'pg';
 const {Pool} = pkg;
 import dotenv from 'dotenv';
 dotenv.config();
-console.log("salida de env", process.env.DATABASE_URL);
 const pool = new Pool({
  connectionString: process.env.DATABASE_URL,
 })
